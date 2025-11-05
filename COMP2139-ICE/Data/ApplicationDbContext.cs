94408933577d6bf60ab15bcd3f76686056b78aac
Judene Brown <judenebrown13@gmail.com>
2025-11-05 10:31:45 -0500

    Initial commit for COMP2139-ICE

---
+++ COMP2139-ICE/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+using COMP2139_ICE.Areas.ProjectManagement.Models;
+using COMP2139_ICE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP2139_ICE.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ProjectTask> ProjectTasks { get; set; }
+
+    
+    //Week6
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Define One-to-Many Relationship: One Project has Many ProjectTasks
+        modelBuilder.Entity<Project>()
+            .HasMany(p => p.Tasks) // One Project has many ProjectTasks
+            .WithOne(t => t.Project) // Each ProjectTask belongs to one Project
+            .HasForeignKey(t => t.ProjectId) // Foreign key in ProjectTask table
+            .OnDelete(DeleteBehavior.Cascade); // Cascade delete ProjectTasks when a Project is deleted
+
+        
+        // Seeding Projects
+        modelBuilder.Entity<Project>().HasData(
+            new Project { ProjectId = 1, Name = "Assignment 1", Description = "COMP2139 Assignment 1" },
+            new Project { ProjectId = 2, Name = "Assignment 2", Description = "COMP2139 Assignment 2" }
+        );
+    }
+}
